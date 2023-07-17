2fec08ff538bdf7db710186a66638f6ff7610d3f
Kivalayu <andryukhacool@mail.ru>
2023-07-17 05:49:37 +0300

    test

---
+++ autotestportf.py
@@ -0,0 +1,39 @@
+import pytest
+import time
+from playwright.sync_api import Playwright, sync_playwright
+
+@pytest.fixture(scope="module", params=["chromium", "firefox"])
+def page(request):
+    with sync_playwright() as playwright:
+        browser_type = getattr(playwright, request.param)
+        browser = browser_type.launch()
+        page = browser.new_page()
+        yield page
+        browser.close()
+
+def test_title(page):
+    try:
+        page.goto("https://www.google.com/?hl=En", timeout=60000)
+        assert page.title() == "Google"
+        print(page.title())
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+def test_buttons(page):
+    try:
+        page.goto("https://www.google.com/?hl=En", timeout=60000)
+        page.get_by_role("button", name="Google Search").click()
+        time.sleep(2)
+        page.get_by_role("combobox", name="Search").click()
+        time.sleep(2)
+        page.get_by_role("button", name="Search by image").click()
+        time.sleep(2)
+        page.get_by_role("button", name="Close").click()
+        time.sleep(2)
+        page.get_by_role("button", name="Search by voice").click()
+        time.sleep(2)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
